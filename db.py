c4accf38080498a8ee4143a75c52ade50cc7430b
zty <zty.apply@outlook.com>
2022-12-31 19:56:07 +0800

    不成功版

--- db.py
+++ db.py
@@ -4,11 +4,29 @@ import sqlite3
 conn = sqlite3.connect('face.db')
 c = conn.cursor()
 
-# 执行 SQL 语句创建表
-c.execute("CREATE TABLE faces (id INTEGER PRIMARY KEY AUTOINCREMENT, face_data BLOB NOT NULL, name TEXT NOT NULL)")
+# # 执行 SQL 语句创建表
+# c.execute("CREATE TABLE faces (id INTEGER PRIMARY KEY AUTOINCREMENT, face_data BLOB NOT NULL, name TEXT NOT NULL)")
 
-# 提交事务
-conn.commit()
+# # 提交事务
+# conn.commit()
+
+# # 关闭数据库连接
+# conn.close()
+
+# 执行 SQL 语句查询所有记录
+c.execute("SELECT name FROM faces")
+
+
+# c.execute("DELETE FROM faces")
+# conn.commit()
+
+
+# 获取所有记录
+records = c.fetchall()
+
+# 遍历记录列表，打印每条记录
+for record in records:
+    print(record)
 
 # 关闭数据库连接
 conn.close()
--- face.py
+++ face.py
@@ -18,10 +18,10 @@ name_entry.pack()
 def record_name(face_data):
     # 获取用户输入的人名
     name = name_entry.get()
+    print(name)
     # 连接数据库
     conn = sqlite3.connect('face.db')
     c = conn.cursor()
-
     # 使用 SQL 语句将人脸数据和人名存储到数据库中
     c.execute("INSERT INTO faces (face_data, name) VALUES (?, ?)", (face_data, name))
     # 提交事务
@@ -29,31 +29,6 @@ def record_name(face_data):
     # 关闭数据库连接
     conn.close()
 
-def get_face():
-    
-    # 使用摄像头捕获人脸图像
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        # 读取摄像头中的帧
-        ret, frame = cap.read()
-    
-        # 使用 Haar 特征分类器检测人脸
-        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
-    
-        # 在图像中画出人脸矩形
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        
-        # 显示人脸图像
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    # 关闭窗口
-    cap.release()
-    cv2.destroyAllWindows()
-    # 连接数据库
 
 def get_face():
     # 使用摄像头捕获人脸图像
@@ -66,34 +41,33 @@ def get_face():
         
         # 使用 Haar 特征分类器检测人脸
         faces = face_cascade.detectMultiScale(frame, 1.3, 5)
-        
-        # 在图像中画出人脸矩形
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # 使用 OpenCV 将人脸图像转换为字符串
+        success, encoded_image = cv2.imencode('.jpg', frame)
+        # 将字符串转换为 bytes 类型
+        face_data = encoded_image.tobytes()
 
         conn = sqlite3.connect('face.db')
         c = conn.cursor()
         # 使用 SQL 语句查询人脸数据对应的人名
         c.execute("SELECT name FROM faces WHERE face_data=?", (face_data,))
         name = c.fetchone()
+        print("Name:",name)
         conn.close()  
-        
-        cv2.putText(frame, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-
+        # 在图像中画出人脸矩形
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(frame, name, (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
         # 显示人脸图像
         cv2.imshow('frame', frame)
         key = cv2.waitKey(1)
         
         # 如果用户按下 'r' 键，调用录入人脸对应人名的函数
-        if key == ord('r'):
-            # 使用 OpenCV 将人脸图像转换为字符串
-            success, encoded_image = cv2.imencode('.jpg', frame)
-            
-            # 将字符串转换为 bytes 类型
-            face_data = encoded_image.tobytes()
-            
+        if key & 0xFF == ord('r'):
+           
             # 调用录入人脸对应人名的函数
             record_name(face_data)
+
         if key & 0xFF == ord('q'):
             # 关闭数据库连接                      
             break
